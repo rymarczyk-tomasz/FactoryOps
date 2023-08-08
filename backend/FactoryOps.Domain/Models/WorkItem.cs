9b6d9ccd1cb11b315a22b6d86f501f4a010096d1
Patryk Wiśniewski <pwisniewski1991@gmail.com>
2023-08-08 21:41:52 +0200

    more docs

--- backend/FactoryOps.Domain/Models/WorkItem.cs
+++ backend/FactoryOps.Domain/Models/WorkItem.cs
@@ -3,7 +3,7 @@ namespace FactoryOps.Domain.Models;
 public class WorkItem
 {
 	/// <summary>
-	/// Unique ID 
+	/// Unique ID.
 	/// </summary>
 	public Guid Id { get; set; }
 
@@ -13,17 +13,29 @@ public class WorkItem
 	public string Name { get; set; } = string.Empty;
 	
 	/// <summary>
-	/// User custom information related to item
+	/// User custom information related to item.
 	/// </summary>
 	public string AdditionalInfo { get; set; } = string.Empty;
 	
 	/// <summary>
 	/// How long many technological hours is needed to finish item.
 	/// </summary>
-	public float TechnologicalHour {get; set; } = 0;
+	public float TechnologicalHour { get; set; } = 0;
+	
+	/// <summary>
+	/// Normative unit which describe how mach time human can work during 8 hour shift
+	/// </summary>
+	public float TechnologicalWorkingFactor{ get; set;} = 7.2f;
 	
 	/// <summary>
 	/// Date when item need to be finished.
 	/// </summary> 
-	public DateTime Deadline {get; set; }
+	public DateTime Deadline { get; set; }
+	
+	/// <summary>
+	/// Date and Time when user plan to start working on item.
+	/// </summary>
+	public DateTime PlanedStart { get; set; }
+	
+
 }
