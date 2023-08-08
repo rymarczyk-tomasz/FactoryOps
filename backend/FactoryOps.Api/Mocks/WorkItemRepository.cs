c489d21ab504272beac466148257d3e3bb00153f
Patryk Wiśniewski <pwisniewski1991@gmail.com>
2023-08-08 21:02:26 +0200

    mock memory db

--- backend/FactoryOps.Api/Mocks/WorkItemRepository.cs
+++ backend/FactoryOps.Api/Mocks/WorkItemRepository.cs
@@ -4,28 +4,38 @@ using FactoryOps.Domain.Ports;
 namespace FactoryOps.Api.Mocks;
 public class WorkItemRepository : IWorkItemsRepository
 {
+	private readonly List<WorkItem> mockMemoryDb;
+	public WorkItemRepository(List<WorkItem> memoryDb)
+	{
+		this.mockMemoryDb = memoryDb;
+	}
 	public Task AddWorkItem(WorkItem item)
 	{
-		throw new NotImplementedException();
+		this.mockMemoryDb.Add(item);
+		return Task.CompletedTask;
 	}
 
 	public Task DeleteWorkItem(string id)
 	{
-		throw new NotImplementedException();
+		var index = this.mockMemoryDb.FindIndex(x => x.Id.ToString() == id);
+		this.mockMemoryDb.Remove(this.mockMemoryDb.ElementAt(index));
+		return Task.CompletedTask;
 	}
 
-	public Task<WorkItem> GetWorkItem(string id)
+	public Task<WorkItem?> GetWorkItem(string id)
 	{
-		throw new NotImplementedException();
+		return Task.FromResult(this.mockMemoryDb.FirstOrDefault(x => x.Id.ToString() == id)); 
 	}
 
 	public Task<WorkItem[]> GetWorkItems()
 	{
-		throw new NotImplementedException();
+		return Task.FromResult(this.mockMemoryDb.ToArray());
 	}
 
 	public Task<WorkItem> UpdateWorkItem(string oldId, WorkItem newItem)
 	{
-		throw new NotImplementedException();
+		var index = this.mockMemoryDb.FindIndex(x => x.Id.ToString() == oldId);
+		if (index == -1) this.mockMemoryDb[index] = newItem;
+		return Task.FromResult(this.mockMemoryDb.ElementAt(index));
 	}
 }
--- backend/FactoryOps.Domain/Ports/IWorkItemsRepository.cs
+++ backend/FactoryOps.Domain/Ports/IWorkItemsRepository.cs
@@ -5,7 +5,7 @@ namespace FactoryOps.Domain.Ports;
 public interface IWorkItemsRepository
 {
 	public Task<WorkItem[]> GetWorkItems();
-	public Task<WorkItem> GetWorkItem(string id);
+	public Task<WorkItem?> GetWorkItem(string id);
 	public Task AddWorkItem(WorkItem item);
 	public Task DeleteWorkItem(string id);
 	public Task<WorkItem> UpdateWorkItem(string oldId, WorkItem newItem);
